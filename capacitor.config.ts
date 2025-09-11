ed678807e310ce10e0808f4111bc1a909aa3b68b
rahul suthar <rahul.suthar@rightbraininfotech.in>
2025-09-11 10:44:12 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'native-call-recorder',
+  webDir: 'dist'
+};
+
+export default config;
